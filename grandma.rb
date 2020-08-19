2be43434ffca0af7c5e6cff5d3bc044d43bfe27f
Red Ebron <redebron20@gmail.com>
2020-08-19 03:24:39 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,10 +2,12 @@
 def speak_to_grandma (phrase)
   if phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == "What did you eat today?".upcase
     return "NO, NOT SINCE 1938!"
+  elsif phrase == "What?".upcase
+    return "NO, NOT SINCE 1938!"
+  else phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
 
   end
 end
