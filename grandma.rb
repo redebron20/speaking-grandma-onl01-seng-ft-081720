29385cdb3c5ec14a812c3ee861c5404ec958ab66
Red Ebron <redebron20@gmail.com>
2020-08-19 03:20:02 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,7 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma (phrase)
-  if phrase == "I LOVE YOU GRANDMA!"
+  if phrase != phrase.upcase
+  elsif phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   end
 end
